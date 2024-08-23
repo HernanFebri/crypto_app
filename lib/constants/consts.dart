04ce9560502c18c3ed17f4333eb4c135fa62f413
Hernan Febri <hernanfebri@gmail.com>
2024-08-23 13:25:19 +0700

    First Commit

---
+++ lib/constants/consts.dart
@@ -0,0 +1,2 @@
+final String CRYPTO_RANK_API_KEY =
+    "10942f8ef1f4e734ac6bce2b833d8c31a8150376d7c8f97f58d0aebd88fa";
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_crypto_app/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        useMaterial3: true,
+      ),
+      routes: {
+        "/home": (context) => HomePage(),
+      },
+      initialRoute: "/home",
+    );
+  }
+}
